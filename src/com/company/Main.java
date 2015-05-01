4081b6ef3960db6fbc6bb12c476701d87cced81b
Nadeem <nadeem95@gmail.com>
2015-05-01 01:13:44 -0500

    change-8

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         // print message
         System.out.println("Hello World !!");
+        System.out.println("This is a change in master.");
     }
 }
